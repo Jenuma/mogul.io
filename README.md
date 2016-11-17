55c3fe04df989f374b9e442c77f29e8aa5a25ed6
Clifton Roberts <Clifton.Roberts@Outlook.com>
2016-11-17 15:40:52 -0600

    :gem: Walk www.regions.com for account balance (#1)
    :key: Walk regions.com for account balance
    
    - Fixed problem where errors weren't printing
    - Added many events and verbose logging for debugging
    - Fixed problem where method invocations were misspelled
    - Worked around issue where `string.includes()` doesn't work in PhantomJS

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # mogul.io
 System of components for tracking my finances.
+
+## Dependencies
+
+Requires CasperJS (which requires PhantomJS). This is NOT a node module.
+
+## Running Locally
+Install PhantomJS.
+Install CasperJS globally: `npm install casperjs -g`
+Add directories for both PhantomJS and CasperJS to PATH.
+Navigate to the directory containing mogul.io and run `casperjs mogul.io.js`
+Data will be written to `output.txt` in the same directory.
+
+## Limitations
+This script was written for my personal use. You are free to try to use it, but you
+will almost certainly have to repurpose it for your needs. It requires a
+`cred.json` file to run which contains account information used to log into the
+service of your choice. Mine is not included for obvious reasons. Here is a sample:
+
+```
+{
+    "url":"https://www.yourbankingservicehere.com",
+    "username":"your_username_here",
+    "password":"your_password_here",
+    "secAnswer1":"your_security_question_1_answer_here",
+    "secAnswer2":"your_security_question_2_answer_here"
+}
+```
--- mogul.io.js
+++ mogul.io.js
@@ -1,25 +1,203 @@
-var request = require("request");
-var cheerio = require("cheerio");
+var casper = require("casper").create();
+var fs = require("fs");
+var cred = require("cred.json");
 
-url = "http://www.imdb.com/title/tt1229340";
+casper.on("resource.requested", function(request) {
+    console.log("Requested: " + request.method + " " + request.url);
+});
 
-request(url, function(error, response, html) {
-    if(!error) {
-        var $ = cheerio.load(html);
-        var title, release, rating;
+casper.on("resource.received", function(resource) {
+    console.log("Received: " + resource.url);
+})
 
-        var headerRegex = /(.+)\s\((\d\d\d\d)\)/;
-        var headerHTML = $("h1").text()
-        var matches = headerRegex.exec(headerHTML);
-        
-        title = matches[1];
-        release = matches[2];
-        
-        // Remove weird newline character.
-        rating = $(".ratingValue").first().text().replace(/\r?\n|\r/, "");
+casper.on("navigation.requested", function(url, navigationType, navigationLocked, isMainFrame) {
+    console.log("Navigation: " + navigationType + " to " + url);
+});
 
-        console.log("Title: " + title);
-        console.log("Released: " + release);
-        console.log("Rating: " + rating);
+casper.on("wait.start", function() {
+    console.log("Started waiting...");
+});
+
+casper.on("wait.done", function() {
+    console.log("Finished waiting...");
+});
+
+casper.on("waitFor.timeout", function(timeout, details) {
+    console.log("Timed out waiting for " + details.selector);
+});
+
+casper.on("step.timeout", function(step, timeout) {
+    console.log("Timed out waiting for " + step);
+});
+
+casper.on("error", function(msg, backtrace) {
+    console.log("Uncaught error: " + msg + " Trace: " + backtrace);
+});
+
+casper.on("step.error", function(err) {
+    console.log("Step failed: " + err);
+});
+
+casper.on("complete.error", function(err) {
+    console.log("Complete callback failed: " + err);
+});
+
+casper.on("run.complete", function() {
+    console.log("Casper completed successfully.");
+});
+
+/*
+ * Start adding to Casper's queue by going to the service's homepage.
+ * Viewport must be set to at least 768w to access desktop layout.
+ */
+casper.start();
+casper.viewport(768, 300);
+casper.thenOpen(cred.url);
+
+/*
+ * Login to the service.
+ */
+casper.then(function() {
+    console.log(casper.getTitle());
+    
+    console.log("Waiting for login form to load...");
+    casper.waitForSelector("form.regions-olb-login-form", login, loginTimeout);
+});
+
+/*
+ * Callback for when the login form loads.
+ */
+function login() {
+    console.log("Inside login() function...");
+    if(casper.exists("input[name='UserName']") &&
+       casper.exists("input[name='OnlineID']") && 
+       casper.exists("input[name='Password']")) {
+            console.log("Login form input fields exist...");
+            casper.fillSelectors("form.regions-olb-login-form", {
+                "input[name='UserName']": cred.username,
+                "input[name='OnlineID']": cred.username,
+                "input[name='Password']": cred.password
+                }, true);
+            console.log("Filled login form and submitted...");    
+    } else {
+        console.log("ERROR: Login form seems invalid.");
+        casper.capture("error-invalid-login-form.png");
+        exit(1);
+    }
+}
+
+/*
+ * Callback for when the login form takes to long to load or doesn't exist.
+ */
+function loginTimeout() {
+    console.log("Inside loginTimeout() function...");
+    console.log("ERROR: Timed out waiting for login form.");
+    casper.capture("error-login-form-timeout.png");
+    exit(1);
+}
+
+/*
+ * Wait for login.
+ */
+casper.then(function() {
+    console.log("Waiting for login response...");
+    
+    if(casper.exists("form.regions-olb-login-form")) {
+        casper.waitWhileVisible("form.regions-olb-login-form", function() {
+            console.log("Login form no longer visible.");
+        }, function() {
+            console.log("ERROR: Timed out waiting for login response.");
+            casper.capture("error-login-submit-timeout.png");
+            exit(1);
+        });
+    } else {
+        console.log("Login form not visible...");
+        casper.capture();
     }
 });
+
+/*
+ * Answer security question, if prompted.
+ */
+casper.then(function() {
+    console.log(casper.getTitle());
+    
+    if(casper.exists("label[for='Answer']")) {
+        console.log("Security question form exists...");
+        
+        var question = casper.fetchText("label[for='Answer']");
+        var answer;
+        
+        if(question == cred.secQuestion1) {
+            console.log("Getting asked security question 1...");
+            answer = cred.secAnswer1;
+        } else if(question == cred.secQuestion2) {
+            console.log("Getting asked security quesiton 2...");
+            answer = cred.secAnswer2;
+        } else if(question == cred.secQuestion3) {
+            console.log("Getting asked security quesiton 3...");
+            answer = cred.secAnswer3;
+        } else {
+            console.log("ERROR: Security question seems invalid.");
+            casper.capture("error-sec-question-invalid.png");
+            exit(1);
+        }
+        
+        if(casper.exists("input[name='Answer']")) {
+            console.log("Security answer input field exists...");
+            casper.fillSelectors("form", {
+                "input[name='Answer']": answer
+                }, true);
+        } else {
+            console.log("ERROR: Security answer input seems invalid.");
+            casper.capture("error-sec-answer-invalid.png");
+            exit(1);
+        }
+    } else {
+        console.log("Security question form doesn't exist...");
+    }
+});
+
+/*
+ * Get balance.
+ */
+casper.then(function() {
+    if(casper.exists(".value")) {
+        console.log("Balance value exists...");
+        
+        var balanceValue = casper.fetchText(".value");
+        balanceValue = balanceValue.match(/\$(?:,?\d{0,3})*\.\d\d/);
+        
+        console.log("Value: " + balanceValue);
+    } else {
+        console.log("ERROR: Balance value seems invalid.");
+        casper.capture("error-balance-invalid");
+        exit(1);
+    }
+});
+
+/*
+ * Log out of the service.
+ */
+casper.thenOpen("https://onlinebanking.regions.com/authentication/signedout");
+
+/*
+ * Run the script and exit when finished.
+ */
+casper.run(function() {
+    console.log("Casper script finished...");
+    exit(0);
+});
+
+function exit(status) {
+    console.log("Exit called...");
+    //casper.exit();
+    //phantom.exit(status);
+}
+
+/*
+ * Writes the passed text to the output file.
+ */
+function write(text) {
+    fs.write("output.txt", text + "\n", "a/+");
+}
